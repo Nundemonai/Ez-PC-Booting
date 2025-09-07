f4c6503b34c075ebda624299aff8152f91d8845c
Nundemonai <74787409+Nundemonai@users.noreply.github.com>
2025-09-07 21:51:21 +0200

    Update conf.py
    Updated for Tuya Lights configuration.

--- conf.py
+++ conf.py
@@ -1,6 +1,27 @@
+######Device Setup#######
 pc_mac_adress="your:pc:mac:adress"
 pc_ip_address="your.pc.local.localip.adress"
 phone_mac_adress="your:phone:mac:adress"
 phone_ip_address="your.phone.localip.adress"
 password = "Your PC SSH Password"
 username = "Your PC login name"
+
+
+##########
+# TUYA API LOGIN
+##########
+
+tuya_ACCESS_ID = "Tuya Access/user ID"
+tuya_ACCESS_KEY = "Tuya Access key"
+tuya_USERNAME = "Your-Tuya-Email-Account"
+tuya_PASSWORD = "Your-Tuya-Password"
+
+tuya_ENDPOINT = "https://openapi.tuyaeu.com" # < Tuya's API endpoint Change this to the server you use.
+
+#######
+# Your lights setup. Change the names of them if you want to. Make sure to change the names in phone_gone.py too.
+#######
+DEVICE1 = "Device-ID1"
+DEVICE2 = "Device-ID2"
+DEVICE3 = "Device-ID3"
+DEVICE4 = "Device-ID4"
